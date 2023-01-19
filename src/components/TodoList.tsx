14ec1ab2b8201cfc54df1ec0da63aa0e8de98298
Everett Lindsay <everettsjunk@gmail.com>
2023-01-18 21:41:28 -0500

    finished with the drag styling

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -21,9 +21,9 @@ const TodoList: React.FC<Props> = ({
 		<div className="container">
 			<Droppable droppableId="TodosList">
 				{
-					(provided) => (
+					(provided, snapshot) => (
 						<div
-							className="todos"
+							className={`todos ${snapshot.isDraggingOver ? "dragactive" : ""}`}
 							ref={provided.innerRef}
 							{...provided.droppableProps}
 						>
@@ -46,9 +46,9 @@ const TodoList: React.FC<Props> = ({
 			</Droppable>
 			<Droppable droppableId="TodoRemove">
 				{
-					(provided) => (
+					(provided, snapshot) => (
 						<div
-							className="todos remove"
+						className={`todos remove ${snapshot.isDraggingOver ? "dragcomplete" : ""}`}
 							ref={provided.innerRef}
 							{...provided.droppableProps}
 						>
